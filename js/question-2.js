8835bb09e57dc7f26bc59a1c2b4a0f939af35f73
Regine <reginenaas@gmail.com>
2022-02-15 11:27:09 +0100

    done

--- js/question-2.js
+++ js/question-2.js
@@ -9,30 +9,28 @@ fetch(url)
     return response.json();
 })
 .then(function(myData){
-    // console.log(myData.results);
+    console.log(myData.results);
     let html = "";
     const data = myData.results;
+    counter = 0;
 
-    // let counter = 1;
-    // for(let item of data) {
-    //     let test =  item.length;
-    //     console.log(item);
-    // }
-
-    for(let i = 0; i < 8; i++) {
+    for(let i of data) {
         html = `
         <div class="container">
-        <img src="${data[i].background_image}" alt="${data[i].name}">
+        <img src="${i.background_image}" alt="${i.name}">
         <ul class="ulList">
-        <li><strong>${data[i].name}</strong></li>
-        <li>Rating: ${data[i].rating}</li>
-        <li>Number of Tags: ${data[i]}</li>
+        <li><strong>${i.name}</strong></li>
+        <li>Rating: ${i.rating}</li>
+        <li>Number of Tags: ${i.tags.length}</li>
         </ul>
         </div>
-        `
+        `;
+        if (i.name === "Play with Gwen") continue; // In order to hield the innocent
         section.innerHTML += html;
+        console.log(i.name);
+        if (++counter >= 8) break;
     } 
-    console.log(data[0]); // HOW DO I GET THE "TAGS"
+    console.log(data[3]);
 })
 .catch(function(error){
     console.error("I think you should come to take a look at this.. ");
@@ -40,3 +38,23 @@ fetch(url)
 .then(function(){
     document.querySelector("#loader").remove();
 });
+
+
+
+
+// Original Code using a different for loop:
+
+
+// for(let i = 0; i < 8; i++) {
+//     html = `
+//     <div class="container">
+//     <img src="${data[i].background_image}" alt="${data[i].name}">
+//     <ul class="ulList">
+//     <li><strong>${data[i].name}</strong></li>
+//     <li>Rating: ${data[i].rating}</li>
+//     <li>Number of Tags: ${data[i].tags.length}</li>
+//     </ul>
+//     </div>
+//     `
+//     section.innerHTML += html;
+// } 
